4d093a01f8bd6260137f638ed2795ddda646e59c
Lady Blue <lady.blue2015@yahoo.com>
2016-01-05 16:07:04 -0800

    updated main.html and json-menu.js

--- js/json-menu.js
+++ js/json-menu.js
@@ -1,14 +1,13 @@
 $(document).ready(function () {
-
-});$.getJSON('/api/businesses', function (data) {
-	var businessList = function (data) {
-		$.each(data, function (key, val) {
-			$(".city").text(val.city + ", " + val.state);
-			$(".distance").text(val.distance);
-			$(".subtitle").text(val.category);
-			$(".buttonText").text(val.businessname);
-			$(".tab").text(val.coupons[0].title);
-		});
-
-	}
+	$.getJSON('/api/businesses', function (data) {
+		var businessList = function (data) {
+			$.each(data, function (key, val) {
+				$(".city").text(val.city + ", " + val.state);
+				$(".distance").text(val.distance);
+				$(".subtitle").text(val.category);
+				$(".buttonText").text(val.businessname);
+				$(".tab").text(val.coupons[0].title);
+			});
+		};
+	});
 });
